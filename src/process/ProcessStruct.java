5e814d91b5f81b743298a71b181bbf1b56462172
xaridar <xaridara@gmail.com>
2025-10-18 18:17:12 -0400

    update to TransRot v1.9.1, add dummy panel to fix mol count bug, use realtime process timers for output

--- src/process/ProcessStruct.java
+++ src/process/ProcessStruct.java
@@ -195,7 +195,7 @@ public class ProcessStruct {
 
     private long getExecTimeLive() {
         if (startTime == -1) startTime = System.currentTimeMillis();
-        Instant inst = handle != null ?
+        Instant inst = (handle != null && status == ProcessStatus.ALIVE) ?
                 handle.info().startInstant().orElse(null) :
                 null;
         if (inst == null) {
@@ -210,7 +210,7 @@ public class ProcessStruct {
     public String getExecTime() {
         if (startTime == -1) startTime = System.currentTimeMillis();
         String ret;
-        Instant inst = handle != null ?
+        Instant inst = (handle != null && status == ProcessStatus.ALIVE) ?
                 handle.info().startInstant().orElse(null) :
                 null;
         if (inst == null) {
@@ -263,29 +263,26 @@ public class ProcessStruct {
                 e.printStackTrace();
             }
             try (Scanner reader = new Scanner(configFile)) {
-                for (int i = 0; i < 2; i++) {
-                    reader.nextLine();
-                }
-                for (int i = 0; i < 21; i++) {
-                    String line = reader.nextLine();
-                    if (!line.contains(":")) break;
-                    String key = line.split(":")[0].split(" \\(true/false\\)")[0];
-                    String value = line.split(":")[1].trim();
-                    if (value.equals("true") || value.equals("false"))
-                        configMap.put(key, value.equals("true"));
-                    else if (value.matches("^[0-9]+$"))
-                        configMap.put(key, Integer.parseInt(value));
-                    else
-                        configMap.put(key, Double.parseDouble(value));
-                }
-                for (int i = 0; i < 3; i++) {
-                    reader.nextLine();
-                }
-                while (reader.hasNextLine()) {
-                    String line = reader.nextLine();
-                    String mol = line.split(" {2,}")[0];
-                    int count = Integer.parseInt(line.split(" {2,}")[1]);
-                    molCounts.put(mol, count);
+                if (!reader.hasNextLine()) return;
+                for (String line = reader.nextLine().trim();; line = reader.nextLine().trim()) {
+                    if (line.matches("^.*:\\s{2,}(\\d+(.\\d+)?|true|false)$")) {
+                        // Setting
+                        String key = line.split(":")[0].split(" \\(true/false\\)")[0];
+                        String value = line.split(":")[1].trim();
+                        if (value.equals("true") || value.equals("false"))
+                            configMap.put(key, value.equals("true"));
+                        else if (value.matches("^[0-9]+$"))
+                            configMap.put(key, Integer.parseInt(value));
+                        else
+                            configMap.put(key, Double.parseDouble(value));
+                    } else if (line.matches("^(\\S+\\s?)+ {2,}\\d+$")) {
+                        // Molecule count
+                        String mol = line.split(" {2,}")[0];
+                        int count = Integer.parseInt(line.split(" {2,}")[1]);
+                        molCounts.put(mol, count);
+                    } else if (line.startsWith("//") || line.length() == 0) continue;
+
+                    if (!reader.hasNextLine()) break;
                 }
             } catch (IOException e) {
                 e.printStackTrace();
